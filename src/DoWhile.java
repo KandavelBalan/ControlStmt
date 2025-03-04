e49cf569cd6adf67d419728537553eadf6628ce9
kanda <kandavelece906@gmail.com>
2025-03-05 00:06:58 +0530

    Added do while

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -1,3 +1,11 @@
 public class DoWhile {
-
+    public static  void main(String args[]){
+        int i=1;
+        int fact=1;
+        do{
+            fact *=i;
+            i++;
+        }while(i<6);
+        System.out.println(fact);
+    }
 }
